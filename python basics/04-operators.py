2e727969b6f490d77b05ce636391ab08f204e269
SYN <postbox.501@yandex.com>
2023-05-21 22:28:30 +0530

    added operators.py on which Arithimetic operators

---
+++ python basics/04-operators.py
@@ -0,0 +1,57 @@
+"""
+What is a operator ?
+operator is a instruction for a program that tells how to execute the code sequentially and generate the desired logic.
+
+python has given below operators :---->
+
+1 -> Arithmetic
+These type of operators are used in doing mathematical calculations.
+
+.==================.=====================================.
+|   Operator       |             Description             |
+|                  |                                     |
+.==================.=====================================.
+|      +           |          used in addition           |
+|      -           |          used in subtraction        |
+|      *           |          used to multiply           |
+|      /           |          used to divide             |
+|      //          |  used to calculate floor division   |
+|      %           |  used to calculate the modulus      |
+|      **          |  used to calculate exponent(power)  |                               
+.==================.=====================================. 
+
+|> difference b/w / and // -------->
+    
+    / (single forward slash) used ot calculate division that mean it returns values in flaot data type.
+    for example 
+    
+    print(10/2)
+    this will return 5.0 and thats a float data type.
+    on the other hand // (double forward slash) returns values in integer format like 3.9 it will return 3.
+    floor division has a con that is sometime for precise calculation we want float data but when we use it,
+    the data is lose by which that mean we cant do exact calculations.
+
+|> what is (%) modulus ?
+    modulus (%) is used to calculate the reminder of of a division.
+
+    print(11 % 2)
+    this line will return 1 that is reminder of 10 / 2.
+
+    NOTE :
+        If we print 11 / 2 that will return 2.5 because it is normal division.
+        If we print 11 // 2 that will print 2 because it is floor division and always return integer not float.
+        If we print 11 % 2 that will return 1 because 1 is the reminder when 11 is not fully divided.
+
+|> what is (**) exponent ?
+    exponent (**) is used to calculate power for example 
+    print(3 ** 2)
+    that means (3)^2 that will print square of 3.
+2 -> Comparison
+Comparison operators are used for comparing two variable value, this is also known as Relational operators.
+
+3 -> Assignment
+4 -> logical
+5 -> Bitwise
+6 -> membership
+7 -> identity
+"""
